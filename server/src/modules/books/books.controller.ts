86a5484feaec794a089a88355ae86e94d05f5321
Manas Manthan Rout <manasmr934@gmail.com>
2026-04-01 17:13:46 +0530

    Role check added for books

--- server/src/modules/books/books.controller.ts
+++ server/src/modules/books/books.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, UploadedFile, UseInterceptors } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put, Request, UploadedFile, UseGuards, UseInterceptors } from "@nestjs/common";
 import { FileInterceptor } from "@nestjs/platform-express";
 import { BooksService } from "./books.service";
 import { CreateBookDto } from "./dto/create-book.dto";
 import { UpdateBookDto } from "./dto/update-book.dto";
+import { AuthGuard } from "@nestjs/passport";
 
 @Controller("books")
 export class BooksController {
@@ -21,26 +22,32 @@ export class BooksController {
     }
 
     @Post("create")
+    @UseGuards(AuthGuard("jwt"))
     @UseInterceptors(FileInterceptor("coverImage"))
     async create(
         @Body() dto: CreateBookDto,
+        @Request() req: any,
         @UploadedFile() file?: any
     ) {
-        return await this.booksService.createBook(dto, file);
+        return await this.booksService.createBook(dto, req, file);
     }
-
+    
     @Put("update/:bookId")
+    @UseGuards(AuthGuard("jwt"))
     @UseInterceptors(FileInterceptor("coverImage"))
     async update(
         @Param("bookId") id: string,
         @Body() dto: UpdateBookDto,
+        @Request() req: any,
         @UploadedFile() file?: any
     ) {
-        return await this.booksService.updateBook(id, dto, file);
+        return await this.booksService.updateBook(id, dto, req, file);
     }
-
+    
     @Delete("delete/:bookId")
-    async delete(@Param("bookId") id: string) {
-        return await this.booksService.deleteBook(id);
+    
+    @UseGuards(AuthGuard("jwt"))
+    async delete(@Param("bookId") id: string,@Request() req: any ) {
+        return await this.booksService.deleteBook(id, req);
     }
 }
